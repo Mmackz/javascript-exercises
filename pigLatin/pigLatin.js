dac515fe1d570a2b783da91f320beaadeb8ea931
Mmackz <matthewmackenz@gmail.com>
2022-03-15 19:05:57 -0700

    ✅ complete tasks

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,6 +1,14 @@
 function pigLatin(string) {
+   const words = string.split(" ");
+   const mapped = words.map((word) => {
+      const indexOfVowel = word.search(/^[aeiou]/);
+      if (indexOfVowel === 0) {
+         return `${word}ay`;
+      }
+      return `${word.slice(indexOfVowel)}${word.slice(0, indexOfVowel)}ay`;
+   });
+   return mapped.join(" ");
+}
 
-};
-  
 // Do not edit below this line
 module.exports = pigLatin;
